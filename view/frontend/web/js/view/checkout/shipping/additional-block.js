618d3b099df0727bca299b331442002b75f289d2
WilcoMonta <111432098+WilcoMonta@users.noreply.github.com>
2023-07-10 11:17:04 +0200

    Bugfix/site url  (#64)
    * fix site url

--- view/frontend/web/js/view/checkout/shipping/additional-block.js
+++ view/frontend/web/js/view/checkout/shipping/additional-block.js
@@ -835,7 +835,7 @@ define(
                 }, loadMap: function () {
                     const useLocator = $('#bh-sl-map-container');
                     const markers = [];
-                    const site_url = createSiteUrl();
+                    const site_url = self.createSiteUrl();
                     $(".montapacking-pickup-service.pickup-option").each(
                         function (index) {
                             const openingtimes = $(this).find(".table-container .table").html();
