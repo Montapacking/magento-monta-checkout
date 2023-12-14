bfb5e97875e3cbf7b25361fb55f397dd9cbb5c24
Wilco Broer <Wilco.Broer@monta.nl>
2023-12-14 15:58:16 +0100

    add Integrationtest

--- Test/Unit/IntegrationTest.php
+++ Test/Unit/IntegrationTest.php
@@ -1,39 +1,45 @@
-<?php
+<?php declare(strict_types=1);
 
-namespace Montapacking\MontaCheckout\Test\Unit;
+namespace Montapacking\MontaCheckout\Test\Integration;
 
 use Magento\Framework\Module\Manager;
 use Monta\CheckoutApiWrapper\MontapackingShipping;
 use Magento\Framework\App\DeploymentConfig;
-
+//
 use PHPUnit\Framework\TestCase;
 use Monta\CheckoutApiWrapper\Objects\Settings;
 
-final class Test extends TestCase
+final class IntegrationTest extends TestCase
 {
-    public function TestGetDeliveryOptionsAndPickupPoints(): void
+    private MontapackingShipping $api;
+
+    public function __construct()
     {
+        parent::__construct();
+
         $settings = new Settings($_ENV['SHOP'], $_ENV['USERNAME'], $_ENV['PASSWORD'], true, 10, $_ENV['GOOGLEAPIKEY'], 2);
 
-        $api = new MontapackingShipping($settings, 'nl-NL');
+        $this->api = new MontapackingShipping($settings, 'nl-NL');
 
-        $api->setAddress(
+        $this->api->setAddress(
             'Papland',
             16,
             '',
             '4206L',
             'Gorinchem',
             '',
-            'NL'
+            'NL' 
         );
 
-        $api->addProduct('croc', 1);
+        $this->api->addProduct('croc', 1);
+    }
 
-        $results = $api->getShippingOptions();
+//    /** @test */
+    public function testGetDeliveryOptionsAndPickupPoints(): void
+    {
+        $results = $this->api->getShippingOptions();
 
         $this->assertTrue(sizeof($results['DeliveryOptions']) > 0);
         $this->assertTrue(sizeof($results['PickupOptions']) > 0);
     }
 }
-
-
