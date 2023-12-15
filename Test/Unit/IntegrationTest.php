38cd9c2a468fe5431b2e769a3a73a2024a67a32f
Wilco Broer <Wilco.Broer@monta.nl>
2023-12-15 11:10:31 +0100

    update test

--- Test/Unit/IntegrationTest.php
+++ Test/Unit/IntegrationTest.php
@@ -13,11 +13,34 @@ final class IntegrationTest extends TestCase
 {
     private MontapackingShipping $api;
 
+    private string $monta_origin;
+    private string $monta_username;
+    private string $monta_password;
+    private bool $monta_enable_pickup_points;
+    private int $monta_max_pickup_points;
+    private string $monta_google_api_key;
+    private float $monta_default_costs;
+
     public function __construct()
     {
         parent::__construct();
+        
+        $this->monta_origin = (string) $_ENV['MONTA_ORIGIN'];
+        $this->monta_username = (string) $_ENV['MONTA_USERNAME'];
+        $this->monta_password = (string) $_ENV['MONTA_PASSWORD'];
+        $this->monta_enable_pickup_points = (bool) $_ENV['MONTA_ENABLE_PICKUP_POINTS'];
+        $this->monta_max_pickup_points = (int) $_ENV['MONTA_MAX_PICKUP_POINTS'];
+        $this->monta_google_api_key = (string) $_ENV['MONTA_GOOGLE_API_KEY'];
+        $this->monta_default_costs = (float) $_ENV['MONTA_DEFAULT_COSTS'];
 
-        $settings = new Settings($_ENV['SHOP'], $_ENV['USERNAME'], $_ENV['PASSWORD'], _ENV['PICKUP_POINTS_ENABLED'], $_ENV['PICKUP_POINTS_MAX_COUNT'], $_ENV['GOOGLEAPIKEY'], $_ENV["DEFAULT_COSTS"]);
+        $settings = new Settings(
+            $this->monta_origin,
+            $this->monta_username,
+            $this->monta_password,
+            $this->monta_enable_pickup_points,
+            $this->monta_max_pickup_points,
+            $this->monta_google_api_key,
+            $this->monta_default_costs);
 
         $this->api = new MontapackingShipping($settings, 'nl-NL');
 
@@ -28,7 +51,7 @@ final class IntegrationTest extends TestCase
             '4206L',
             'Gorinchem',
             '',
-            'NL' 
+            'NL'
         );
 
         $this->api->addProduct('croc', 1);
